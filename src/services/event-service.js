2d574f0431883f3f4869b4cefb542628c53e499d
Android Studio <devandroid@ort>
2024-05-13 16:43:14 -0300

    yuco

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -5,16 +5,19 @@ export default class EventService{
         const repo = new EventRepository();
         var stringsql = 'WHERE '
         if(name != null){
-            stringsql += `name = '${name}' AND`
+            stringsql += `name = '${name}' AND `
         }
-        else if(category != null){
-            stringsql += `id_event_category = ${category} AND`
-        }else if(startdate != null){
-            stringsql += `start_date = '${startdate}' AND`
-        }else if(tag != null){
-            stringsql += `tag = '${name}' AND`
+        if(category != null){
+            console.log("llego")
+            stringsql += `id_event_category = ${category} AND `
         }
-        let sqlfinal = stringsql.substring(0,((stringsql.length)-4))
+        if(startdate != null){
+            stringsql += `start_date = '${startdate}' AND `
+        }
+        if(tag != null){
+            stringsql += `tag = '${name}' AND `
+        }
+        let sqlfinal = stringsql.substring(0,((stringsql.length)-5))
         let returnArray = await repo.getAsync(sqlfinal);
         return returnArray;
     }
